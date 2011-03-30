62e141b47751f27885c900a508df3c10746ac9ed
Petr Onderka <gsvick@gmail.com>
2011-03-30 21:06:34 +0200

    cleanup (mostly removed unused usings)

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+﻿using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace WP_ITN_RSS
 {
--- Global.asax.cs
+++ Global.asax.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace WP_ITN_RSS
@@ -12,12 +8,6 @@ namespace WP_ITN_RSS
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            /*routes.MapRoute(
-                "Default", // Route name
-                "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );*/
 
             routes.MapRoute(
                 "Default",
